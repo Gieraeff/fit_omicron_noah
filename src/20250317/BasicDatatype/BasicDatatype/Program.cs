4b57e1375363d460a65bd64291adbd582d374f19
Noah Zech (WIFI) <gieraeff.ding@gmail.com>
2025-03-17 21:35:13 +0100

    Exampel Data types

---
+++ src/20250317/BasicDatatype/BasicDatatype/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicDatatype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int => integer values
+
+            //declaration  int16,32 and 64 are for the betriebsystem
+            int age;
+
+            //initalize
+
+            age = 1;
+            
+            //declaration & initialize
+            int count = 0;
+
+
+
+            Console.WriteLine("Age: " + age.ToString());
+
+            //dezimal values
+            double weight = 10.50;
+
+            //money
+            decimal money = 15.99m;      // 128 bit memory
+
+
+        }
+    }
+}
+    
