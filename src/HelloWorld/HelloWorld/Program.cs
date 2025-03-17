053f00986b73296d09013f558675a992e0990a4e
Noah Zech (WIFI) <gieraeff.ding@gmail.com>
2025-03-17 19:50:43 +0100

    This is our first example - hello world
    This is our first example - hello world

---
+++ src/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;       //system does not just mean include all
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+comments for course
+
+ctrl + spacebar for fullfiment
+Menue -> View -> error list 
+
+*/
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
