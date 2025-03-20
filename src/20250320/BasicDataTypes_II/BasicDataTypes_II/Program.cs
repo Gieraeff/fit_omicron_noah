cc6d1a89c8aaa99c0e50d039dad6dae669ec5f3f
Noah Zech (WIFI) <gieraeff.ding@gmail.com>
2025-03-20 18:32:01 +0100

    Example for Basic Datatype

---
+++ src/20250320/BasicDataTypes_II/BasicDataTypes_II/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicDataTypes_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //String
+            string name = "Gandalf";
+            //name = string.Empty;
+
+            char sign = 'a';
+
+            bool isPowerOn = false;
+
+
+
+        }
+    }
+}
