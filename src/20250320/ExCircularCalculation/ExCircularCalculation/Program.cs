62f9971329603c32ef528fb8c94ea2eb4805d82a
Noah Zech (WIFI) <gieraeff.ding@gmail.com>
2025-03-20 18:32:40 +0100

    Example for calculation an circel

---
+++ src/20250320/ExCircularCalculation/ExCircularCalculation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExCircularCalculation
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // variable declaration & initialisation
+            double diameter = 15.12;
+            double circumference = 0.0; // umfang 
+
+            // calculation
+            circumference = diameter * Math.PI;
+
+            // output
+            Console.WriteLine("The Circumference of my circle is: " + circumference.ToString());
+
+
+
+        }
+    }
+}
