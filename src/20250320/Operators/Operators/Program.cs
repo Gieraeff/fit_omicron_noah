95e892a29456b4fead1ea56542ec4a59959ba919
Noah Zech (WIFI) <gieraeff.ding@gmail.com>
2025-03-20 18:33:16 +0100

    Example an documantation for some Operators

---
+++ src/20250320/Operators/Operators/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // arithmetic operators
+            // + - * / %
+
+            //Math functions
+            //Math.
+
+            // composite operator
+            int number = 0;
+
+            number = number + 5;
+
+            number += 5;
+            number *= 2;
+
+            number += 1;
+            number++;   //increment
+            number--;   //decrement
+
+            //++number vs. number++
+            //pre-increment vs. post-increment
+
+            Console.WriteLine("Number: " + number++);   // first the number gets put out and then in summs
+                                                        // ++number summs and then puts it out
+                                                        // just don t mix pre and post
+
+
+
+
+        }
+    }
+}
