d7a00e4aa5cfcb2ac57d0534b8067efe85a2da95
Noah Zech (WIFI) <gieraeff.ding@gmail.com>
2025-03-27 20:56:00 +0100

    Basics about interation and there suage

---
+++ src/20250327/Iterations/Iterations/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Step: " + i.ToString());
+            }
+
+            DateTime now = DateTime.Now;
+
+            while (DateTime.Now.Hour == 20)
+            {
+                //...
+                // do something
+                
+            }
+
+            int w = 0;
+
+            do
+            {
+                Console.WriteLine("Step:" + w.ToString());
+                w++;
+            }
+            while (w < 5);
+        }
+    }
+}
