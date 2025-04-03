0ab065b794aa42996be93e17846e004d61f82c6c
Noah Zech (WIFI) <gieraeff.ding@gmail.com>
2025-04-03 21:35:37 +0200

    learned about placeholder

---
+++ src/20250403/PlaceholderInStrings/PlaceholderInStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaceholderInStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            int maxnumber = 10;
+
+            Console.WriteLine("Here were" + number +" items found.");
+            Console.WriteLine("{0} items of {1} found.", number, maxnumber);
+
+            Console.WriteLine($"{number} items of {maxnumber} are found.");
+
+            string name = $"Noah-{DateTime.Now.Date}";
+            
+        }
+    }
+}
