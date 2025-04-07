8266c8e1dcc3c933f69d56863dc72a91e4c644be
Noah Zech(HOME) <gieraeff.ding@gmai.com>
2025-04-07 19:53:58 +0200

    introduction foreach

---
+++ src/20250407/foreachIntroduction/foreachIntroduction/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foreachIntroduction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[] { "Gandalf", "Max Musterman", "Golum", "Emor" };
+
+            foreach(string name in names)
+            {
+                Console.WriteLine($"Name: {name}");
+            }
+        }
+    }
+}
