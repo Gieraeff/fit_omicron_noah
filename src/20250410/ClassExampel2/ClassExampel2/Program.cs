cf8f06ab3557e1da8833ca7a3b01278b3d02bb7e
Noah Zech (WORK) <gieraeff.ding@gmail.com>
2025-04-10 21:42:57 +0200

    learning about properties, classes and more

---
+++ src/20250410/ClassExampel2/ClassExampel2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExampel2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string userinput = String.Empty;
+
+            Vehicle car1 = new Vehicle("Pegasus",200);
+
+            //car1.Description = "ford mustang green edition";
+            //car1.MaxSpeed = 300;
+            ////car1.CurrentSpeed = 0;
+            //car1.Color =ConsoleColor.Green;
+
+            car1.Display();
+
+
+            //userinput = Console.ReadLine();
+            car1.SpeedUp(100);
+
+            car1.Display();
+
+            
+            car1.SpeedUp(120);
+
+
+            car1.Color = ConsoleColor.DarkMagenta;
+            Console.WriteLine(car1.Color);
+
+            int speed = car1.CurrentSpeed * 150;
+            car1.Display();
+
+        }
+    }
+}
+
---
+++ src/20250410/ClassExampel2/ClassExampel2/Vehicle.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExampel2
+{
+    internal class Vehicle
+    {
+        // private object with underline lowercase
+        private string _description;
+        private int _currentSpeed;
+        private int _maxSpeed;
+        private ConsoleColor _color;
+
+        //constructor ==> to create a valid object => Semantic consistency 
+
+
+        // standard constructor
+        //public Vehicle() 
+        //{
+
+        //}
+
+        // sepzific constructor
+
+        public Vehicle(string description, int maxspeed, ConsoleColor color)
+        {
+            _color = color;
+            _currentSpeed = 0;
+
+            _description = description;
+            _maxSpeed = maxspeed;
+        }
+
+        public Vehicle(string description, int maxspeed)
+        {
+            _color = ConsoleColor.Green;
+            _currentSpeed = 0;
+
+            _description = description;
+            _maxSpeed = maxspeed;
+        }
+
+        public int CurrentSpeed
+        {
+            get
+            {
+                return _currentSpeed;
+            }
+        }
+
+        public ConsoleColor Color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                if (value!= ConsoleColor.Black)
+                {
+                    _color = value; 
+                }
+            }
+        }
+
+        //java style programming
+        //public int Get_CurrentSpeed()
+        //{
+        //    return CurrentSpeed;
+        //}
+
+        public void SpeedUp(int delta)
+        {
+            if ((_currentSpeed + delta) <= _maxSpeed)
+            {
+                _currentSpeed += delta;
+            }
+            else
+            {
+                _currentSpeed = _maxSpeed;
+            }
+        }
+
+        public void Display()
+        {
+
+            ConsoleColor oldColor = Console.ForegroundColor;
+
+            Console.ForegroundColor = _color;
+
+            Console.WriteLine($"{_description} - [{_currentSpeed} / {_maxSpeed} km/h]");
+
+
+        }
+
+    }
+}
