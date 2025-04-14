3d4d73752f7e3d0edc65c2f8de40f67af8239c5b
Noah Zech(HOME) <gieraeff.ding@gmai.com>
2025-04-14 21:44:27 +0200

    adapting the class and var

---
+++ src/20250414/VehicelRentalApplication/VehicelRentalApplication/Car.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace VehicelRentalApplication
+{
+    public class Car
+    {
+        private string _description;
+        private int _currentSpeed;
+        private int _maxSpeed;
+        private string _brand;
+        private int _enginePower;
+
+        public Car(string description, string brand, int maxSpeed, int enginePower)
+        {
+            _currentSpeed = 0;
+            _description = description;
+            _maxSpeed = maxSpeed;
+            _enginePower = enginePower;
+            _brand = brand;
+        }
+
+        public int EnginePower
+        {
+            get { return _enginePower; }
+        }
+
+        public int MaxSpeed
+        {
+            get { return _maxSpeed; }
+        }
+
+        public int CurrentSpeed
+        {
+            get
+            {
+                return _currentSpeed;
+            }
+        }
+
+        public string Brand
+        {
+            get { return _brand; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+        }
+
+
+        //user spezific constructor
+        //public Car(string description, int maxSpeed)
+        //{
+        //    _currentSpeed = 0;
+        //    _description = description;
+        //    _maxSpeed = maxSpeed;            
+        //}
+
+        public bool SpeedUp(int delta)
+        {
+            if (_currentSpeed + delta <= _maxSpeed)
+            {
+                _currentSpeed += delta;
+                return true;
+            }
+
+            return false;
+        }
+
+        public string GetInfoString()
+        {
+            string info = $"{_description}-{_brand}";
+            info += $"\n{_enginePower}-[{_currentSpeed} / {_maxSpeed} km/h]";
+
+            return info;
+        }
+    }
+}
