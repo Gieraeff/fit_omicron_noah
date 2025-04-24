628a890a5070b6d8ec1a15aa72f6d70e2e219a48
Noah Zech (WIFI) <gieraeff.ding@gmail.com>
2025-04-24 21:19:37 +0200

    Interface example

---
+++ src/20250424/ForcedPolymorphism/ForcedPolymorphism/IShape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForcedPolymorphism
+{
+    public interface IShape
+    {
+        string Name { get; }
+       
+        int Vertices { get; }
+       
+        void Draw();
+
+    }
+}
