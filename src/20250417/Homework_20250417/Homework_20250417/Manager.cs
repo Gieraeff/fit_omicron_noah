6b7e3ebf5636eca3b6de314754f69e96aeab43b7
Noah Zech (WORK) <gieraeff.ding@gmail.com>
2025-04-24 16:34:01 +0200

    added Manager class

---
+++ src/20250417/Homework_20250417/Homework_20250417/Manager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_20250417
+{
+    public class Manager : Employee
+    {
+
+        public  Manager(string Name, GenderState Gender, DateTime Birthday)
+                : base(Name, Gender, Birthday)
+        {
+
+        }
+    }
+}
