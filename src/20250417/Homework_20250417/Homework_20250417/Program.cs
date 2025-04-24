29e7383ba8a9b124b4974db5ef6580c725758dd1
Noah Zech (WORK) <gieraeff.ding@gmail.com>
2025-04-24 16:35:39 +0200

    started project

---
+++ src/20250417/Homework_20250417/Homework_20250417/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_20250417
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
