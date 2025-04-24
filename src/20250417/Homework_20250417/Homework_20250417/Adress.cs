dc93fd9e936871e24abcef028174c07499c37bb7
Noah Zech (WORK) <gieraeff.ding@gmail.com>
2025-04-24 16:35:08 +0200

    implemented Adress class

---
+++ src/20250417/Homework_20250417/Homework_20250417/Adress.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_20250417
+{
+    class Adress
+    {
+		private string _street;
+		private int _number;
+		private int _postalCode;
+		private string _city;
+		private string _country;
+
+		public string Country
+		{
+			get { return _country; }
+		}
+
+		public string City
+		{
+			get { return _city; }
+		}
+
+
+		public int PostalCode
+		{
+			get { return _postalCode; }
+		}
+
+
+		public int Number
+		{
+			get { return _number; }
+		}
+
+		public string Street
+		{
+			get { return _street; }
+		}
+
+		public string GetInfoString()
+		{
+			string info = $"Street: {_street} Number: {_number} PostalCode: {_postalCode} City:{_city} Country: {_country}";
+			return info;
+		}
+
+	}
+}
