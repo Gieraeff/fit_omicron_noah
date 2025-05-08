3581a43aa95fac766157c9b68ba8ec794d2f1ce2
Noah Zech (WIFI) <gieraeff.ding@gmail.com>
2025-05-08 19:07:10 +0200

    LINQ

--- src/20250508/Lambda_Linq/Lambda_Linq/Program.cs
+++ src/20250508/Lambda_Linq/Lambda_Linq/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,12 +18,85 @@ namespace Lambda_Linq
 
             doSomething("Calling a existing mehtod...");
 
-            if (doSomething != null)
-            {
-                doSomething("Calling a existing mehtod...");
-            }
+            //if (doSomething != null)
+            //{
+            //    doSomething("Calling a existing mehtod...");
+            //}
             //is the same loke do Something with the question mark
-            doSomething?.Invoke("Calling a existing mehtod...");
+            //doSomething?.Invoke("Calling a existing mehtod...");
+
+            doSomething = delegate(string msg)
+            {
+                Console.WriteLine(msg); 
+                //Console.Beep();
+            };
+
+            doSomething("Chimpansini bananini");
+
+            // Introduction to lambda
+            doSomething = (string msg) => { Console.WriteLine(msg); };
+
+            doSomething("yurr");
+
+            doSomething = (msg) => Console.WriteLine(msg);
+            doSomething = msg => Console.WriteLine(msg);
+
+            doSomething("HEllo");
+
+            //example
+            var valueList = new List<int> {5, 8, 10, 2, 15, 7, 20, 29 };
+            
+            var filteredValues = FilterToOdd(valueList);
+
+            var evenValues = Filter(valueList, x => x % 2 == 0);
+
+            var greaterThen10Values = Filter(valueList, x => x > 10);
+
+            //greaterThen10Values = Filter(valueList, GreaterThen8);
+
+            // LINQ
+            var result = valueList.Where(x => x > 5)
+                                  .Select(x => 2 * x);
+        }
+
+        private static bool GreaterThen8(int value)
+        {
+            if (value > 8)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static IEnumerable<T> Filter<T>(IEnumerable<T> valueList, Predicate<T> filterCondition)
+        {
+            var resultList = new List<T>();
+
+            foreach (var value in valueList)
+            {
+                if (filterCondition(value))
+                {
+                    resultList.Add(value);
+                }
+            }
+
+            return resultList;
+        }
+
+        private static IEnumerable<int> FilterToOdd(List<int> valueList)
+        {
+            var resultList = new List<int>();
+
+            foreach (var value in valueList)
+            {
+                if (value % 2 != 0)
+                {
+                    resultList.Add(value);
+                }
+            }
+
+            return resultList;
         }
 
         private static void WriteColoredMessage(string message)
