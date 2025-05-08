185dbaf10bcddf4e04d7dcceba061166953e6daf
Noah Zech (WIFI) <gieraeff.ding@gmail.com>
2025-05-08 17:45:02 +0200

    Lambda example

---
+++ src/20250508/Lambda_Linq/Lambda_Linq/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> doSomething;
+
+            doSomething = WriteColoredMessage;
+
+            doSomething("Calling a existing mehtod...");
+
+            if (doSomething != null)
+            {
+                doSomething("Calling a existing mehtod...");
+            }
+            //is the same loke do Something with the question mark
+            doSomething?.Invoke("Calling a existing mehtod...");
+        }
+
+        private static void WriteColoredMessage(string message)
+        {
+            ConsoleColor oldColor = Console.ForegroundColor;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine(message);
+
+            Console.ForegroundColor = oldColor;
+        }
+    }
+}
