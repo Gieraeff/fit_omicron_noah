110398b95fe8f10747edc3280611923a7a3505f0
Noah Zech(HOME) <gieraeff.ding@gmai.com>
2025-05-12 20:49:43 +0200

    Events basic, definition and usage

---
+++ src/20250512/Event_Basic/Event_Basic/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> messageAction = ShowMessage;
+
+            messageAction("Yurr Yurr");
+
+            var shapesList = new List<Shape>
+            {
+                new Shape("Circle",int.MaxValue),
+                new Shape("Rectangle", 4),
+                new Shape("Hexagon", 6)
+            };
+
+
+            //make some subscriptions
+            foreach (var shape in shapesList)
+            {
+                shape.ShapeIsDrawn += Shape_ShapeIsDrawn;
+            }
+
+            foreach (var shape in shapesList)
+            {
+                shape.Draw();
+            }
+
+            //remove some subscriptions
+            //foreach (var shape in shapesList)
+            //{
+            //    shape.ShapeIsDrawn -= Shape_ShapeIsDrawn;
+            //}
+
+        }
+
+        private static void Shape_ShapeIsDrawn(object sender, EventArgs e)
+        {
+            var shape = sender as Shape;
+            if (shape == null)
+            {
+                return;
+            }
+
+            Console.WriteLine("EVENT:" + shape.Description + "is drawn.");
+        }
+
+        static void ShowMessage(string messageToShow)
+        {
+            Console.WriteLine(messageToShow.ToUpper());
+        }
+    }
+}
---
+++ src/20250512/Event_Basic/Event_Basic/Shape.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Basic
+{
+    public class Shape
+    {
+		private string _description;
+		private int _vertexCount;
+
+		public event EventHandler ShapeIsDrawn;
+
+        public Shape(string description, int vertices)
+        {
+			_description = description;
+			_vertexCount = vertices;
+        }
+
+        public int VertexCount
+		{
+			get { return _vertexCount; }
+		}
+
+
+		public string Description
+		{
+			get { return _description; }
+		}
+
+		public void Draw()
+		{
+            Console.WriteLine($"Drawing the shape: {_description},...");
+
+			//fire the event if it is not null
+			ShapeIsDrawn?.Invoke(this, new EventArgs()); // _description + " is drawn."
+        }
+
+	}
+}
