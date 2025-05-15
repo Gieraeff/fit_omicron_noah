ed8b0f1c474e50d3fadb6314452ccd3becbfad87
Noah Zech (WORK) <gieraeff.ding@gmail.com>
2025-05-15 18:46:32 +0200

    implemented the class

---
+++ src/20250515/Projekt_Playlist/Projekt_Playlist/MP3Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Playlist
+{
+    public class MP3Item
+    {
+        public string _titel;
+        public string _artist;
+        public DateTime _duration;
+        public Image _image;
+
+        // todo path integration
+
+
+        public Image Image { get; private set; }
+
+        public DateTime Duration { get; private set; }
+
+        public string Artist { get; private set; }
+        public string Titel { get; private set; }
+    }
+}
