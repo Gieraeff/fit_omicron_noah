32a05ae8c1fc1673ad990580fb1c0ad72e45a705
Noah Zech (WORK) <gieraeff.ding@gmail.com>
2025-05-15 18:46:11 +0200

    implemented the class playlsit

---
+++ src/20250515/Projekt_Playlist/Projekt_Playlist/Playlist.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Projekt_Playlist
+{
+    public class Playlist
+    {
+        private string _name;
+        private string _autor;
+        private List<string> _itemList;
+
+
+        public string Name
+        {
+            get{ return _name; }
+        }
+
+        public string Autor
+        {
+            get { return _autor; }
+        }
+
+        public void Add(string name, string autor)
+        {
+            string adder = name;
+            adder += autor;
+            _itemList.Add(adder);
+        }
+
+        public void Remove(string name)
+        {
+            _itemList.Remove(name);
+        }
+
+        public void Clear()
+        {
+
+        }
+        public void Load()
+        {
+
+        }
+
+        public void Save()
+        {
+
+        }
+
+    }
+}
