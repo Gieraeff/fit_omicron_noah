7b9d4c60ee8f3f4eb110b346988ba0311478f580
Noah Zech (WORK) <gieraeff.ding@gmail.com>
2025-05-15 18:46:56 +0200

    implemented the class M3uRepository

---
+++ src/20250515/Projekt_Playlist/Projekt_Playlist/M3uRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekt_Playlist
+{
+    public class M3uRepository
+    {
+        public string _description;
+        // todo Extension
+
+        public string Description { get; private set; }
+    }
+}
