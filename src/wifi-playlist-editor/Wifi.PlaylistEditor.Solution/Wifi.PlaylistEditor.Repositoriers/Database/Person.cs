23be40b5b4781fb41247597b9e3a09aa2cac1a07
Noah Zech(HOME) <gieraeff.ding@gmai.com>
2025-05-19 21:40:31 +0200

    Database classes

---
+++ src/wifi-playlist-editor/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.Repositoriers/Database/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+
+namespace Wifi.PlaylistEditor.Repositoriers.Database
+{   
+        public class Person
+        {
+            [BsonId]
+            [BsonRepresentation(BsonType.ObjectId)]
+            public string Id { get; set; }
+
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Age { get; set; }
+        }
+
+   
+}
