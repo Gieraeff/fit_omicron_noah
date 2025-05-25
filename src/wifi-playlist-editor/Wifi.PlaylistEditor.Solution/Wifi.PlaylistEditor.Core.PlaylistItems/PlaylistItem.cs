6a388639c5b5dab03865825354d59db51c095d2c
Noah Zech(HOME) <gieraeff.ding@gmai.com>
2025-05-25 22:35:27 +0200

    some things

---
+++ src/wifi-playlist-editor/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.Core.PlaylistItems/PlaylistItem.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Id3;
+
+namespace Wifi.PlaylistEditor.Core.PlaylistItems
+{
+    public class PlaylistItem
+    {
+
+        public void setPlaylistMP3Item(IPlaylistItem item)
+        {
+
+            string[] musicFiles = Directory.GetFiles(item.Path);
+            foreach (string musicFile in musicFiles)
+            {
+                using (var mp3 = new Mp3(musicFile))
+                {
+                    Id3Tag tag = mp3.GetTag(Id3TagFamily.Version2X);
+
+                }
+            }
+        }
+    }
+}
